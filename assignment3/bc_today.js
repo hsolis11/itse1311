49dcff45d37a579db22bc2f61de32b5cb0a48f89
Hector Solis <hsolis11@outlook.com>
2021-02-19 10:25:34 -0600

    change Date(2018, 9, 18) to Date()

--- assignment3/bc_today.js
+++ assignment3/bc_today.js
@@ -16,7 +16,7 @@
 
 "use strict";
 
-var thisDate = new Date(2018, 9, 12);
+var thisDate = new Date();
 var dateString = thisDate.toLocaleDateString();
 var dateHTML = "<h2>" + dateString + "</h2>";
 var thisDay = thisDate.getDay();
