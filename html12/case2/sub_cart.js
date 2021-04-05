37436277a0a6a83ed4d249a7638841c6406912ec
Hector Solis <hsolis11@outlook.com>
2021-04-05 13:44:18 -0500

    complete

--- html12/case2/sub_cart.js
+++ html12/case2/sub_cart.js
@@ -5,8 +5,8 @@
    Tutorial 12
    Case Problem 2
 
-   Author: 
-   Date:   
+   Author: Hector Solis
+   Date:   04/04/2021   
 
    Filename: sub_cart.js
 
@@ -22,3 +22,43 @@
 
 */
 
+window.addEventListener("load", setupCart);
+
+function setupCart(){
+   // define the event handlers fo the Add to Order buttons on the page.
+   var addButtons = document.getElementsByClassName("addButton");
+
+   for(var i=0; i < addButtons.length; i++){
+      addButtons[i].addEventListener("click", addItem);
+   }
+}
+
+
+function addItem(){
+   // adds items to the shopping cart on the page.
+   var foodItem = event.target.nextElementSibling;
+   var foodID = foodItem.getAttribute("id");
+   var foodDescription = foodItem.cloneNode(true);
+   var cartBox = document.getElementById("cart");
+   var duplicateOrder = false;
+
+   for(var i=0; i < cartBox.childNodes.length; i++){
+      
+      if (foodID == cartBox.childNodes[i].id){
+         cartBox.childNodes[i].firstChild.value += 1;
+         cartBox.childNodes[i].firstChild.innerHTML = cartBox.childNodes[i].firstChild.value;
+         duplicateOrder = true;
+         break;
+      }
+   }
+
+   if (duplicateOrder == false){
+      var orderCount = document.createElement("span");
+      orderCount.textContent = 1;
+      orderCount.value = 1;
+      foodDescription.prepend(orderCount);
+      cartBox.append(foodDescription);
+   }
+
+   
+}
